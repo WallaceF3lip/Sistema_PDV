11f0551ab026af9ec202f7b56dd04bb9335d73bc
Wallace <wallace@stallos.com.br>
2026-06-30 21:24:17 -0300

    Ajuste Vercel - front, API_URL

--- pdv_View/src/environments/environment.ts
+++ pdv_View/src/environments/environment.ts
@@ -1,5 +1,6 @@
+declare var process: any;
+
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000',
-  // apiUrl: 'http://wallace-stallos:8000',
+  apiUrl: process.env['API_URL'] as string,
 };
